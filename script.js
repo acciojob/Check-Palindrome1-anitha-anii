3fae57abcd50be7de90de1fef71d38a9229ffe85
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-17 10:51:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
-
+let String="race a car";
 function palindrome(str){
-
+  str.replace(/\s/g, "");
+  let i=0;
+	let j=str.length-1;
+	while (i<j) {
+		if (str[i]===str[j]) {
+			return true;
+		}
+		i++;
+		j--;
+		}
+	return false;
 }
+console.log(palindrome(String));
 module.exports = palindrome
