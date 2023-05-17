cd5d84f8cc91017dc84d844b0d168384cc508dd8
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-17 10:59:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-  str.replace(/\s/g, "");
-  let i=0;
-	let j=str.length-1;
-	while (i<j) {
-		if (str[i]===str[j]) {
-			return true;
-		}
-		i++;
-		j--;
-		}
-	return false;
+  const alphanumericOnly = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+ 
+  const reversed = alphanumericOnly.split('').reverse().join('');
+
+  return alphanumericOnly === reversed;
 }
 
 module.exports = palindrome
